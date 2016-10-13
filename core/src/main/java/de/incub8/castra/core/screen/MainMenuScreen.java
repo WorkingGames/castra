f03139faf2c37209bae7cea5087f7cf40916f853
Volker Ivens <volkerivens@incub8.de>
2016-10-13 15:29:03 +0200

    Introducing Screens (CST-4)

---
+++ core/src/main/java/de/incub8/castra/core/screen/MainMenuScreen.java
@@ -0,0 +1,49 @@
+package de.incub8.castra.core.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.incub8.castra.core.Castra;
+
+public class MainMenuScreen extends ScreenAdapter
+{
+    public MainMenuScreen(Castra game)
+    {
+        this.game = game;
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+    }
+
+    private final Castra game;
+    private final SpriteBatch batch;
+    private final BitmapFont font;
+
+    @Override
+    public void render(float delta)
+    {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.getCamera().update();
+
+        batch.setProjectionMatrix(game.getCamera().combined);
+        batch.begin();
+        font.draw(batch, "Welcome to Charge", 600, 350);
+        font.draw(batch, "Tap anywhere to begin!", 600, 400);
+        batch.end();
+
+        if (Gdx.input.isTouched())
+        {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void dispose()
+    {
+        batch.dispose();
+        font.dispose();
+    }
+}
