0015d60305ff739569cb338f6a59d984b1b613b8
Volker Ivens <volkerivens@incub8.de>
2016-10-14 09:22:29 +0200

    initial static worldbuilder (CST-6)

--- core/src/main/java/de/incub8/castra/core/model/SettlementSize.java
+++ core/src/main/java/de/incub8/castra/core/model/SettlementSize.java
@@ -1,15 +1,19 @@
 package de.incub8.castra.core.model;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import com.badlogic.gdx.graphics.Texture;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public enum SettlementSize
 {
-    SMALL(1, null), MEDIUM(2, null), LARGE(3, null);
+    SMALL(1, null, 50, 28), MEDIUM(2, null, 70, 39), LARGE(3, null, 90, 50);
 
     private final int spawnAmount;
     private final Texture texture;
+    private final int width;
+    private final int height;
 }
