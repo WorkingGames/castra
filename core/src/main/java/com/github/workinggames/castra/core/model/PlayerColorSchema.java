f6502ef661463c86d6bb94010f88112a5d0a6dc9
Volker Ivens <volkerivens@incub8.de>
2020-05-27 15:22:08 +0200

    more primary colors, who needs secondary anyway

--- core/src/main/java/com/github/workinggames/castra/core/model/PlayerColorSchema.java
+++ core/src/main/java/com/github/workinggames/castra/core/model/PlayerColorSchema.java
@@ -8,12 +8,19 @@ import com.badlogic.gdx.graphics.Color;
 @RequiredArgsConstructor
 public enum PlayerColorSchema
 {
-    RED(new PlayerColor(new Color(0xda0205ff), new Color(0x6d0103ff))),
-    BLUE(new PlayerColor(new Color(0x4d7afdff), new Color(0x023adaff))),
-    GREEN(new PlayerColor(new Color(0x32cd32ff), new Color(0x006400ff))),
+    RED(new PlayerColor(new Color(0xFF0000ff), new Color(0x6d0103ff))),
+    BLUE(new PlayerColor(new Color(0x0000FFff), new Color(0x023adaff))),
+    GREEN(new PlayerColor(new Color(0x7CFC00ff), new Color(0x006400ff))),
     YELLOW(new PlayerColor(new Color(0xffff00ff), new Color(0xffd700ff))),
-    PURPLE(new PlayerColor(new Color(0x9370dbff), new Color(0x8a2be2ff))),
-    BLACK(new PlayerColor(new Color(0x383838ff), new Color(0x171717ff))),
+    PURPLE(new PlayerColor(new Color(0x800080ff), new Color(0x8a2be2ff))),
+    ORANGE(new PlayerColor(new Color(0xFFA500ff), new Color(0x171717ff))),
+    CYAN(new PlayerColor(new Color(0x00FFFFff), new Color(0x171717ff))),
+    PINK(new PlayerColor(new Color(0xFFC0CBff), new Color(0x171717ff))),
+    DARK_GREY(new PlayerColor(new Color(0x383838ff), new Color(0x171717ff))),
+    LIGHT_BLUE(new PlayerColor(new Color(0x00BFFFff), new Color(0x171717ff))),
+    DARK_BLUE(new PlayerColor(new Color(0x00008Bff), new Color(0x171717ff))),
+    DARK_GREEN(new PlayerColor(new Color(0x006400ff), new Color(0x171717ff))),
+    KHAKI(new PlayerColor(new Color(0xF0E68Cff), new Color(0x171717ff))),
     NEUTRAL(new PlayerColor(new Color(0xc8c8c8ff), new Color(0x4b4b4bff)));
 
     @Getter
