691d6154c49b4e1cfc3e0c29602522b07c3924cd
Volker Ivens <volkerivens@incub8.de>
2020-05-28 12:28:01 +0200

    got rid of second player color

--- core/src/main/java/com/github/workinggames/castra/core/model/PlayerColor.java
+++ core/src/main/java/com/github/workinggames/castra/core/model/PlayerColor.java
@@ -8,38 +8,31 @@ import com.badlogic.gdx.utils.ObjectMap;
 @Data
 public class PlayerColor
 {
-    private static final Color REPLACEMENT_COLOR1 = Color.valueOf("ff00ffff");
-    private static final Color REPLACEMENT_COLOR2 = Color.valueOf("960096ff");
+    private static final Color REPLACEMENT_COLOR = Color.valueOf("ff00ffff");
 
     private final ObjectMap<Color, Color> colorMapping;
     private final String regionNameSuffix;
 
-    public PlayerColor(Color color1, Color color2)
+    public PlayerColor(Color color)
     {
-        colorMapping = createColorMapping(color1, color2);
-        regionNameSuffix = createRegionNameSuffix(color1, color2);
+        colorMapping = createColorMapping(color);
+        regionNameSuffix = createRegionNameSuffix(color);
     }
 
     public Color getPrimaryColor()
     {
-        return colorMapping.get(REPLACEMENT_COLOR1);
+        return colorMapping.get(REPLACEMENT_COLOR);
     }
 
-    public Color getSecondaryColor()
-    {
-        return colorMapping.get(REPLACEMENT_COLOR2);
-    }
-
-    private ObjectMap<Color, Color> createColorMapping(Color color1, Color color2)
+    private ObjectMap<Color, Color> createColorMapping(Color color)
     {
         ObjectMap<Color, Color> result = new ObjectMap<>();
-        result.put(REPLACEMENT_COLOR1, color1);
-        result.put(REPLACEMENT_COLOR2, color2);
+        result.put(REPLACEMENT_COLOR, color);
         return result;
     }
 
-    private String createRegionNameSuffix(Color color1, Color color2)
+    private String createRegionNameSuffix(Color color)
     {
-        return color1.toString() + color2.toString();
+        return color.toString();
     }
 }
